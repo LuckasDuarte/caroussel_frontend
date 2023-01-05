c30b18f53758fd160f2ee6944c8a0c6a84cd4298
Lucas Duarte <luckasdu686@gmail.com>
2023-01-04 21:25:31 -0300

    Subindo Repositorio

---
+++ script.js
@@ -0,0 +1,20 @@
+let count = 1;
+
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    // Funcao para Passar as Imagens
+    nextImage();
+}, 5000 )
+
+function nextImage(){
+    count++;
+
+    if (count> 4 ){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+
+
+}
